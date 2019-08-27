f17bed8849658c2e38ec7cef67ff06b87e69d769
gonzaemon111 <goki14914@icloud.com>
2019-08-28 00:20:13 +0900

    init commit

---
+++ db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+    "github.com/jinzhu/gorm"
+    "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
+    "github.com/asuforce/gin-gorm-tutorial/entity"
+)
+
+var (
+    db  *gorm.DB
+    err error
+)
+
+// Init is initialize db from main function
+func Init() {
+    db, err = gorm.Open("postgres", "host=0.0.0.0 port=5432 user=gorm dbname=gorm password=gorm sslmode=disable")
+    if err != nil {
+        panic(err)
+    }
+    autoMigration()
+}
+
+// GetDB is called in models
+func GetDB() *gorm.DB {
+    return db
+}
+
+func autoMigration() {
+  db.AutoMigrate(&entity.User{})
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "github.com/asuforce/gin-gorm-tutorial/db"
+  "github.com/asuforce/gin-gorm-tutorial/server"
+  )
+
+func main() {
+    db.Init()
+    server.Init()
+}
